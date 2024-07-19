2214d7b2120f8dd1f89717a15d78ff4f89c65796
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-07-19 21:48:55 +0530

    create a factory for singleton transport and avoid multiple instances of the same transport class

---
+++ src/factories/transport-factory.ts
@@ -0,0 +1,25 @@
+import { MailTransport } from "../mail";
+import { Transport } from "../types/transport";
+
+const transport: Transport[] = [];
+
+export function createTransport(transportType: "mail" | "sms") {
+  switch (transportType) {
+    case "mail": {
+      const cachedTransport = transport.find(
+        (item) => item instanceof MailTransport,
+      );
+      if (cachedTransport) return cachedTransport;
+      const mailTransport = new MailTransport();
+      transport.push(mailTransport);
+      return mailTransport;
+    }
+
+    case "sms":
+      console.log("SMS do not support yet");
+      break;
+
+    default:
+      console.log(`${transportType} does not support yet`);
+  }
+}
