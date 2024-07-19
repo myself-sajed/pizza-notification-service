1e4c30f0d6cd69895abdf711103043714eb5a481
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-07-19 21:48:18 +0530

    create mail transport to send emails

---
+++ src/mail.ts
@@ -0,0 +1,31 @@
+import nodemailer, { Transporter } from "nodemailer";
+import { Message, Transport } from "./types/transport";
+import config from "config";
+import logger from "./config/logger";
+
+export class MailTransport implements Transport {
+  private transporter: Transporter;
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      host: config.get("transport.mail.host"),
+      port: config.get("transport.mail.port"),
+      secure: config.get("transport.mail.secure"),
+      auth: {
+        user: config.get("transport.mail.auth.user"),
+        pass: config.get("transport.mail.auth.pass"),
+      },
+    });
+  }
+
+  async send(message: Message) {
+    const info = await this.transporter.sendMail({
+      from: config.get("transport.mail.from"),
+      to: message.to,
+      subject: message.subject,
+      text: message.text,
+      html: message.html,
+    });
+
+    logger.info({ info, message });
+  }
+}
